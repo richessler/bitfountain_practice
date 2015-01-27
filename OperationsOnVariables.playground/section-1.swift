17cd20b1cbee61d15ad7e93356620532354f3afa
Richard Hessler <richessler@gmail.com>
2015-01-27 13:18:25 -0500

    updated playground with some string stuff

--- OperationsOnVariables.playground/section-1.swift
+++ OperationsOnVariables.playground/section-1.swift
@@ -2,22 +2,35 @@
 
 import UIKit
 
-var x = 10
+let x = 10
 
-var disc = 0.3
+let disc = 0.3
 
-var price:Double = 33
+let price:Double = 33
 
-var total = price * (1 - disc)
+let total = price * (1 - disc)
 
 var string1 = "Richard"
 var string2 = "Hessler"
 
-var fullName = "\(string1) \(string2)"
+let fullName = "\(string1) \(string2)"
 
 fullName.uppercaseString
 fullName.lowercaseString
 
-var numberString = "9"
-var numberStrToInt = numberString.toInt()
+let numberString = "9"
+let numberStrToInt = numberString.toInt()
 var optToInt = numberStrToInt! // "!" unwraps the optional
+
+optToInt = optToInt + 3
+// same as optToInt += 3
+
+let dblStr = "3.3598"
+
+// Tell swift to use dblStr as a String
+// take the String, assuming correct double format, and turn to a objective-C double(doubleValue)
+// Double coerces doubleValue to be a Swift Double class.
+var dblValFrmStr = Double((dblStr as NSString).doubleValue)
+
+dblValFrmStr += 3.234
+
